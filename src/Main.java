8e2e17f9199dc6d5c7ac9db669267ff8d1c881c5
Alexander Nikitin <alexnik000@yandex.ru>
2023-02-25 14:51:21 +0300

    Improved benchmark.

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,21 @@ import algorithm.IntSequencePair;
 import algorithm.LevenshteinDiff;
 import algorithm.SequenceEqualityMap;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class Main {
     private static final Random random = new Random();
     private static final int LENGTH = 10000;
+    private static final int ITERATIONS = 20;
     private static final int[] s1 = new int[LENGTH];
     private static final int[] s2 = new int[LENGTH];
     private static SequenceEqualityMap compare;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //System.in.read();
+        System.out.println("Start!");
         long t0 = System.currentTimeMillis();
-        for(int j = 0; j < 20; j++) {
+        for(int j = 0; j < ITERATIONS; j++) {
             for (int i = 0; i < LENGTH; i++) {
                 s1[i] = random.nextInt(LENGTH / 100);
                 s2[i] = random.nextInt(LENGTH / 100);
